8002d173c4291513afb7b708808c204fa30eeb12
Cyril <hourdebaigt_cyril@orange.fr>
2026-04-07 09:53:47 +0200

    Ajout de la route PUT /user/{id}

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -13,6 +13,7 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class UserController extends AbstractController
 {
@@ -83,6 +84,82 @@ final class UserController extends AbstractController
                 ],
             ],
             Response::HTTP_CREATED
+        );
+    }
+
+    #[Route('/user/{id}', name: 'app_user_update', methods: ['PUT'])]
+    #[IsGranted('ROLE_ADMIN', message: 'Vous n’avez pas les droits suffisants pour modifier un utilisateur.')]
+    public function updateUser(
+        int $id,
+        Request $request,
+        UserRepository $userRepository,
+        SerializerInterface $serializer,
+        ValidatorInterface $validator,
+        UserPasswordHasherInterface $passwordHasher,
+        EntityManagerInterface $entityManager
+    ): JsonResponse {
+        // On recherche l'utilisateur à modifier en base de données
+        $user = $userRepository->find($id);
+
+        // Si l'utilisateur n'existe pas, on retourne une erreur 404
+        if (!$user) {
+            return new JsonResponse(['error' => 'Utilisateur introuvable.'], Response::HTTP_NOT_FOUND);
+        }
+
+        // On transforme le JSON reçu en tableau PHP
+        $data = json_decode($request->getContent(), true);
+
+        // Si le JSON est invalide, on retourne une erreur 400
+        if ($data === null) {
+            return new JsonResponse(['error' => 'Le JSON envoyé est invalide.'], Response::HTTP_BAD_REQUEST);
+        }
+
+        // On met à jour uniquement les champs présents dans la requête
+        if (array_key_exists('login', $data)) {
+            $user->setLogin($data['login']);
+        }
+
+        if (array_key_exists('city', $data)) {
+            $user->setCity($data['city']);
+        }
+
+        // On valide l'utilisateur après modification
+        $errors = $validator->validate($user);
+
+        if ($errors->count() > 0) {
+            return new JsonResponse($serializer->serialize($errors, 'json'), Response::HTTP_BAD_REQUEST, [], true);
+        }
+
+        // On vérifie qu'un autre utilisateur n'utilise pas déjà ce login
+        if (array_key_exists('login', $data)) {
+            $existingUser = $userRepository->findOneBy(['login' => $data['login']]);
+
+            if ($existingUser && $existingUser->getId() !== $user->getId()) {
+                return new JsonResponse(['error' => 'Ce login est déjà utilisé.'], Response::HTTP_BAD_REQUEST);
+            }
+        }
+
+        // Si un nouveau mot de passe est envoyé, on le hash avant l'enregistrement
+        if (array_key_exists('password', $data) && !empty($data['password'])) {
+            $hashedPassword = $passwordHasher->hashPassword($user, $data['password']);
+            $user->setPassword($hashedPassword);
+        }
+
+        // On enregistre les modifications
+        $entityManager->flush();
+
+        // On retourne une réponse JSON de succès
+        return new JsonResponse(
+            [
+                'message' => 'Utilisateur modifié avec succès.',
+                'user' => [
+                    'id' => $user->getId(),
+                    'login' => $user->getLogin(),
+                    'city' => $user->getCity(),
+                    'roles' => $user->getRoles(),
+                ],
+            ],
+            Response::HTTP_OK
         );
     }
 }
